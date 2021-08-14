5745a3494c848fe96e07809b0f56d1ad018773c5
xavier45564 <eazzyme1@gmail.com>
2021-08-14 02:18:41 -0400

    edit

--- thestrengthprogram/settings.py
+++ thestrengthprogram/settings.py
@@ -101,6 +101,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
