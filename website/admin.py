0f536f6bac9a870a085d0a86a263646461ba94ce
xavier45564 <eazzyme1@gmail.com>
2021-08-27 20:47:42 -0400

    edit

--- website/admin.py
+++ website/admin.py
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Resume)
-=======
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(Resume)
->>>>>>> 622b10ad29f0794ec8e93dd5d3194e62a85e474b
 admin.site.register(Newsletter)
