e34ad9b43322bef8074b4987efad0f2e709bb44b
xavier45564 <eazzyme1@gmail.com>
2021-08-27 20:40:15 -0400

    edit

--- website/models.py
+++ website/models.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 from django.db import models
 from django.forms.widgets import Media
-
-
 
 # Create your models here.
 class Resume(models.Model):
@@ -21,28 +18,4 @@ class Newsletter(models.Model):
     email = models.EmailField(max_length=200)
     date = models.DateField(auto_now=True)
     def __str__(self):
-=======
-from django.db import models
-from django.forms.widgets import Media
-
-
-
-# Create your models here.
-class Resume(models.Model):
-    name = models.CharField(max_length=200)
-    email = models.CharField(max_length=200, blank=True)
-    phone = models.IntegerField(blank=True, default=None)
-    subject = models.CharField(max_length=200)
-    resume = models.FileField(default=None, upload_to='documents/')
-    message = models.CharField(max_length=500)
-    date = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-    
-class Newsletter(models.Model):
-    email = models.EmailField(max_length=200)
-    date = models.DateField(auto_now=True)
-    def __str__(self):
->>>>>>> 622b10ad29f0794ec8e93dd5d3194e62a85e474b
         return self.email
